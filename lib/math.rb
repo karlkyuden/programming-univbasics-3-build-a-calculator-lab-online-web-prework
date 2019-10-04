9575d0e341c4ee47fd3c919dc635028f47de2159
Karl Jerrenny Ojeda <karlojeda@yahoo.com>
2019-10-04 17:53:48 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -13,13 +13,13 @@ def division(num1, num2)
 end
 
 def multiplication(num1, num2)
-
+  return product = num1 * num2
 end
 
 def modulo(num1, num2)
-
+  return modulo = module(num1/num2)
 end
 
 def square_root(num)
-
+  return squareRoot = Math.squareRoot(num)
 end
